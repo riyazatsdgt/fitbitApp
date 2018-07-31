183d58ab1cb61d22e537f0436dbb6aec92b8e537
LAPTOP-SQCASSM3\BBS <riyaj@sdglobaltech.com>
2018-07-31 17:43:27 +0800

    modification

--- app.js
+++ app.js
@@ -22,29 +22,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+// app.use('/users', users);
+app.get('/ehr/app/healthkit/fitbit/auth/:userId', function (req, res,next) {
+  res.send({ status: 'done', userId: req.params.userId }).status(200);
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 
-app.get('/ehr/app/healthkit/:userId',function(req,res){
-  res.send({status:'done',userId:req.params.userId}).status(200);
-});
+
+
+
 module.exports = app;
